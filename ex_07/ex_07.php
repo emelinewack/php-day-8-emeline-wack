c9e565d3bf7aaa3bd134825895dbb120327d75b7
Emeline Wack <emy@Emys-MacBook-Pro.local>
2021-07-07 15:01:29 +0200

    first

--- ex_07/ex_07.php
+++ ex_07/ex_07.php
@@ -1,6 +1,9 @@
 <?php
 
-function modify_cookie() {
-
+function modify_cookie($cookie, $value) {
+    if(isset($_COOKIE[$cookie])) {
+        unset($_COOKIE[$cookie]); 
+        setcookie($cookie, $value, time()+3600, "/");
+    } 
 }
 ?>
