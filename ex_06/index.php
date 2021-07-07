eb36eb9b7b812394519e7085c1a24cd1553efd38
Emeline Wack <emy@Emys-MacBook-Pro.local>
2021-07-07 14:05:16 +0200

    forst

--- ex_06/index.php
+++ ex_06/index.php
@@ -2,8 +2,8 @@
 
 function remove_cookie($cookie) {
     isset($_COOKIE['remember_user']) ;
-        unset($_COOKIE[$cookie]); 
-        setcookie($cookie, null, -1);
+    unset($_COOKIE[$cookie]); 
+    setcookie($cookie, null, -1);
     
 }
 ?>
