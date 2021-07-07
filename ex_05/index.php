502f2926d3ca74000c60d84d770f95b11655aca0
Emeline Wack <emy@Emys-MacBook-Pro.local>
2021-07-07 11:56:09 +0200

    bon

--- ex_05/index.php
+++ ex_05/index.php
@@ -13,29 +13,12 @@ session_start();
     }
 
     if ($name) {
+        setcookie("name", $name);
         echo "Hello " . $name;
     }
 
     else{
         echo "Hello platypus";
     }
-
-session_close();
-    if(array_key_exists("name", $_GET)) {
-        $name = $_GET["name"];
-        $_COOKIE["name"]=$name;
-    }
-
-    elseif(array_key_exists("name", $_COOKIE)) {
-        $name = $_COOKIE["name"];
-    }
-
-    if ($name) {
-        echo "Hello " . $name;
-    }
-
-    else{
-        echo "Hello platypus";
-    }
-
+    
 ?>
