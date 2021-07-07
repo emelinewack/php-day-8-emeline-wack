b9eb56a587cd51cae7eb4a38ad40aa438e0d9a68
Emeline Wack <emy@Emys-MacBook-Pro.local>
2021-07-07 17:26:34 +0200

    first

---
+++ ex_08/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Hi bitch</title>
+    </head>
+    <body>
+    
+<? if($name = $_POST["name"]);
+
+?>
+
+
+        <form method="post" action="index.php">
+        Name : <input type ="text" name="name" size="12"><br>
+        <input type="submit" value="Submit">
+        
+    </body>
+</html>
