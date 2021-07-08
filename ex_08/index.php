e23d36eaf677079713edf978c6a45b2753900907
Emeline Wack <emy@Emys-MacBook-Pro.local>
2021-07-08 12:03:20 +0200

    bon

--- ex_08/index.php
+++ ex_08/index.php
@@ -5,14 +5,16 @@
     </head>
     <body>
     
-<? if($name = $_POST["name"]);
-
-?>
-
-
+        <?php
+            if(isset($_POST["name"])) {
+                echo "Hi " . $_POST["name"] . ", enjoy this page!";
+            }
+            else{
+        ?>
         <form method="post" action="index.php">
-        Name : <input type ="text" name="name" size="12"><br>
-        <input type="submit" value="Submit">
-        
+            Name : <input type ="text" name="name" size="12"><br>
+            <input type="submit" value="Submit">
+        </form>
+       <?php }?>
     </body>
 </html>
