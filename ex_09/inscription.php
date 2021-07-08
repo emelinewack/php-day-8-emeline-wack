9d1ad68ba4ef0c31a1a9f6409a47820b49cb32c0
Emeline Wack <emy@Emys-MacBook-Pro.local>
2021-07-08 17:14:09 +0200

    pas bon

---
+++ ex_09/inscription.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <title>inscription.php</title>
+    </head>
+    <body>
+    
+        <form method="post" action="index.php">
+            Name : <input type ="text" name="name" size="12"><br>
+            <input type="submit" value="Submit">
+
+
+        method="post" action="index.php">
+            Email : <input type ="text" email="email" size="12"><br>
+            <input type="submit" value="Submit">
+
+
+        method="post" action="index.php">
+            Password : <input type ="text" password="password" size="12"><br>
+            <input type="submit" value="Submit">
+
+      
+        method="post" action="index.php">
+            Password_confirmation : <input type ="text" password="password" size="12"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
